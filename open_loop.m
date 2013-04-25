90988a4bcf61e8bcc06f8c0e07e9cce21ddf7ccc
aceisScope <bertieliu52@gmail.com>
2013-04-25 20:16:41 +0800

    compute parameters

--- open_loop.m
+++ open_loop.m
@@ -7,14 +7,36 @@ fclose(fid);
 
 data = c{1,2};
 cputime = data(:,2);
-
-current_t = data (:,3);
-detector_t = data (:,4);
+starttime = cputime (1);
+cputime = cputime - starttime;
+current_t = data (:,3); %cold finger
+detector_t = data (:,4); %dector
+tec_current = data (:,5);
 
 figure(1);
 title('Sine Function');
 plot(cputime,current_t,'r',cputime,detector_t,'b');
-xlabel('time');
-ylabel('temperature');
-axis([cputime(1),cputime(end),0,detector_t(end)]);
+xlabel('time(sec)');
+ylabel('temperature(celcius)');
+axis([cputime(1),cputime(end),current_t(1),current_t(end)]);
+
+t2 = cputime(1000);
+y2 = current_t(1000);
+t1 = cputime (160);
+y1 = current_t(160);
+y_stable = current_t(end);
+K = y_stable/tec_current(1);
+T = (t2 - t1)/ (log(1-y1/y_stable) - log(1-y2/y_stable));
+tau = (t2*log(1-y1/y_stable) - t1*log(1-y2/y_stable))/(log(1-y1/y_stable) - log(1-y2/y_stable)); %maybe there's no delay
+
+%gs=tf([1],[1 1],'ioDelay',tau);
+H = tf([1],[1 1]);
+
+check = [];
+for i = 1:length(cputime)
+    check = [check, K*exp(-tau*cputime(i))/(T*cputime(i)+1)];
+end
+hold on;
+plot(cputime,check,'g');
+
 
